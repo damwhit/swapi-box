6c0eb2f937ab36972ce81b65100d7acc78086076
David Whitaker <damwhit@gmail.com>
2018-10-29 22:52:43 -0600

    Add iteration 1 random film scrolling text feature
    also saving results to storage
    used canonical react ajax call found here https://reactjs.org/docs/faq-ajax.html#where-in-the-component-lifecycle-should-i-make-an-ajax-call

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Scroll = (props) => {
-  return(
-    <div>
-      Scroll
-    </div>
-  );
+class Scroll extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      films: []
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('films')) {
+      this.setState({
+        isLoaded: true,
+        films: JSON.parse(localStorage.getItem('films')),
+      });
+      return;
+    }
+    fetch("https://swapi.co/api/films/")
+      .then(res => res.json())
+      .then(
+        (res) => {
+          localStorage.setItem('films', JSON.stringify(res.results));
+          this.setState({
+            isLoaded: true,
+            films: res.results
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, films } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      const randomFilm = films[Math.floor(Math.random() * films.length)];
+      return (
+        <article>
+          <p>{randomFilm.opening_crawl}</p>
+          <h3>{randomFilm.title}</h3>
+          <h4>{randomFilm.release_date}</h4>
+        </article>
+      );
+    }
+  }
 }
 
 export default Scroll;
-
