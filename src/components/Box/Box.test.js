444db73733b324dd4539d17ef8b4a5c01c1d6e16
David Whitaker <damwhit@gmail.com>
2018-12-10 17:51:20 -0700

    Add another test to test toggling a favorite resource

--- src/components/Box/Box.test.js
+++ src/components/Box/Box.test.js
@@ -3,23 +3,37 @@ import { shallow } from 'enzyme';
 import Box from './Box';
 
 describe('Box', () => {
-  it('should update the state with the number of favorite resources', () => {
-    // Setup
-    const wrapper = shallow(<Box />);
-    const mockResources = [
-      { name: 'Han Solo', isFavorite: true },
-      { name: 'Chewbacca', isFavorite: true },
-      { name: 'Luke Skywalker', isFavorite: false },
-    ];
+  const mockResources = [
+    {name: 'Alderaan', category: 'planets', isFavorite: true},
+    {name: 'Dagobah', category: 'planets', isFavorite: false},
+    {name: 'Endor', category: 'planets', isFavorite: false},
+    {name: 'Earth', category: 'planets', isFavorite: true},
+  ];
+  const wrapper = shallow(<Box />);
 
+  beforeEach(() => {
     wrapper.setState({
       resources: mockResources,
     });
+  });
 
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update the state with the number of favorite resources', () => {
     // Execution
     wrapper.instance().countFavorites();
 
     // Expectation
-    expect(wrapper.state('numFavorites')).toEqual(2);
+    expect(wrapper.state('numFavorites')).toBe(2);
+  });
+
+  it('should update a resource based on it\'s name', () => {
+    expect(wrapper.state('resources')[1].isFavorite).toBe(false);
+
+    wrapper.instance().toggleFavoriteResource('Dagobah');
+
+    expect(wrapper.state('resources')[1].isFavorite).toBe(true);
   });
 });
